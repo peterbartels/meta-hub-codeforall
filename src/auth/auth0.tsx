169beb277d385d2876296f69588c1f06303220aa
Peter Bartels <peterbartels@gmail.com>
2020-04-22 17:58:24 +0800

    feat(header): show menu items according to isAuthenticated

--- src/auth/auth0.tsx
+++ src/auth/auth0.tsx
@@ -37,7 +37,6 @@ export const Auth0Provider = ({
 
       if (isAuthenticated) {
         const user = await auth0FromHook.getUser();
-        console.log(user)
         setUser(user);
       }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -53,12 +53,14 @@ const HeaderComponent: FunctionComponent = () => {
             <span>Home</span>
           </MenuItem>
         </Link>
-        <Link to="/my-profile">
-          <MenuItem onClick={dispatchStartAddProfile}>
-            <FontAwesomeIcon icon="user" size="2x" color="#0289C8" />
-            <span>My Profile</span>
-          </MenuItem>
-        </Link>
+        {isAuthenticated && (
+          <Link to="/my-profile">
+            <MenuItem onClick={dispatchStartAddProfile}>
+              <FontAwesomeIcon icon="user" size="2x" color="#0289C8" />
+              <span>My Profile</span>
+            </MenuItem>
+          </Link>
+        )}
         <Link to="/profiles">
           <MenuItem onClick={dispatchStartAddProfile}>
             <FontAwesomeIcon icon="user-cog" size="2x" color="#0289C8" />
@@ -71,14 +73,18 @@ const HeaderComponent: FunctionComponent = () => {
             <span>Organisations</span>
           </MenuItem>
         </Link>
-        <MenuItem onClick={() => loginWithRedirect({})}>
-          <FontAwesomeIcon icon="user-cog" size="2x" color="#0289C8" />
-          <span>Login</span>
-        </MenuItem>
-        <MenuItem onClick={() => logoutWithRedirect()}>
-          <FontAwesomeIcon icon="user-cog" size="2x" color="#0289C8" />
-          <span>Logout</span>
-        </MenuItem>
+        {!isAuthenticated && (
+          <MenuItem onClick={() => loginWithRedirect({})}>
+            <FontAwesomeIcon icon="user-cog" size="2x" color="#0289C8" />
+            <span>Login</span>
+          </MenuItem>
+        )}
+        {isAuthenticated && (
+          <MenuItem onClick={() => logoutWithRedirect()}>
+            <FontAwesomeIcon icon="user-cog" size="2x" color="#0289C8" />
+            <span>Logout</span>
+          </MenuItem>
+        )}
       </MenuContainer>
     </NameContainer>
   </>)
