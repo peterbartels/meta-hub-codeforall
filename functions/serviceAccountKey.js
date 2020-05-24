ec46969d7044ad8ae6471cb6225c3c2eba0da540
Peter Bartels <peterbartels@gmail.com>
2020-05-24 16:11:50 +0800

    add service account key

---
+++ functions/serviceAccountKey.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "type": "service_account",
+  "project_id": "hacker-hub",
+  "private_key_id": process.env.FIRESTORE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIRESTORE_PRIVATE_KEY,
+  "client_email": process.env.FIRESTORE_CLIENT_EMAIL,
+  "client_id": process.env.FIRESTORE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.FIRESTORE_CLIENT_CERT
+}
